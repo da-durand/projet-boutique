ac9cb69d9a6f50dd2f2b4059563742d68c8f6357
dadurand <ha.halo@live.fr>
2019-10-30 10:46:30 +0100

    accueil1

---
+++ static/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $(".nav-button").click(function(){
+        if ($("header nav ul").css("display") == "none"){
+            $("header nav ul").removeClass("d-none");
+        }
+        else{
+            $("header nav ul").addClass("d-none");
+        }
+    })
+
+
+
+})
