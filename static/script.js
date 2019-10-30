9b42cc347173f9d501dfdb827d69d563e5e9e746
dadurand <ha.halo@live.fr>
2019-10-30 14:20:12 +0100

    catalog

--- static/script.js
+++ static/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
     $(".nav-button").click(function(){
+        
         if ($("header nav ul").css("display") == "none"){
             $("header nav ul").removeClass("d-none");
         }
         else{
             $("header nav ul").addClass("d-none");
         }
-    })
 
+    })
 
 
 })
