a6d5cbc71dc8d8cd8467d4aed16c6c7d987d225d
dadurand <ha.halo@live.fr>
2019-10-31 12:27:57 +0100

    push

--- static/catalog.js
+++ static/catalog.js
@@ -4,7 +4,7 @@ var product0 = {
     description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et egestas augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum maximus eros, a condimentum diam dapibus non. Ut tempus sapien at blandit hendrerit. Mauris vel vehicula orci. Donec non risus eu diam accumsan ornare a vel nunc. Etiam porta enim nisi, sit amet imperdiet lectus commodo eget. Nulla magna ex, dapibus sed rhoncus nec, auctor et ligula. In malesuada ante neque, quis condimentum sapien faucibus eget. Vestibulum in interdum ipsum. Duis ut lorem velit. Aenean congue cursus neque, eget egestas turpis mattis non. Mauris quis diam aliquet, blandit justo id, cursus eros. Sed et vulputate velit. Integer finibus lacus vel porta laoreet.",
     price : 45,
     quantity : 7,
-    thumb : "http://placehold.it/140x140",
+    thumb : "./static/img/image.png",
     pictures : ["http://placehold.it/400x400", "http://placehold.it/400x400", "http://placehold.it/400x400", "http://placehold.it/400x400"],
 }
     
@@ -14,7 +14,7 @@ var product1 = {
     description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et egestas augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum maximus eros, a condimentum diam dapibus non. Ut tempus sapien at blandit hendrerit. Mauris vel vehicula orci. Donec non risus eu diam accumsan ornare a vel nunc. Etiam porta enim nisi, sit amet imperdiet lectus commodo eget. Nulla magna ex, dapibus sed rhoncus nec, auctor et ligula. In malesuada ante neque, quis condimentum sapien faucibus eget. Vestibulum in interdum ipsum. Duis ut lorem velit. Aenean congue cursus neque, eget egestas turpis mattis non. Mauris quis diam aliquet, blandit justo id, cursus eros. Sed et vulputate velit. Integer finibus lacus vel porta laoreet.",
     price : 22,
     quantity : 6,
-    thumb : "http://placehold.it/140x140",
+    thumb : "./static/img/SCshop.png",
     pictures : ["http://placehold.it/400x400", "http://placehold.it/400x400", "http://placehold.it/400x400", "http://placehold.it/400x400"],
 }
     
@@ -24,7 +24,7 @@ var product2 = {
     description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et egestas augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum maximus eros, a condimentum diam dapibus non. Ut tempus sapien at blandit hendrerit. Mauris vel vehicula orci. Donec non risus eu diam accumsan ornare a vel nunc. Etiam porta enim nisi, sit amet imperdiet lectus commodo eget. Nulla magna ex, dapibus sed rhoncus nec, auctor et ligula. In malesuada ante neque, quis condimentum sapien faucibus eget. Vestibulum in interdum ipsum. Duis ut lorem velit. Aenean congue cursus neque, eget egestas turpis mattis non. Mauris quis diam aliquet, blandit justo id, cursus eros. Sed et vulputate velit. Integer finibus lacus vel porta laoreet.",
     price : 33,
     quantity : 7,
-    thumb : "http://placehold.it/140x140",
+    thumb : "./static/img/cyber-punk2077.jpg",
     pictures : ["http://placehold.it/400x400", "http://placehold.it/400x400", "http://placehold.it/400x400", "http://placehold.it/400x400"],
 }
     
--- static/script.js
+++ static/script.js
@@ -19,15 +19,29 @@ $(document).ready(function(){
     for (let i = 0; i < catalog.length ; i++){
 
         // genération du catalogue
-        $(".card-container").append('<div class="cards-'+i+' col-12 col-md-6 col-lg-4">')
-        $('.cards'+i).append('<div class="card-body row">')
-        $(".cards"+i+" .card-body").append('<div class="col-12"> <a href="./product.html" class="text-decoration-none text-light"> </div>')
-        $(".cards"+i+" .card-body a").append('<img src="'+ catalog[0].thumb +'" alt="" class="mw-100"></img>')
-        $(".cards"+i+" .card-body a").append('<h4>'+ catalog[0].name +'</h4>')
-        $(".cards"+i+" .card-body").append('<div class="col-6">' + catalog[0].price + ' €</div>')
-        $(".cards"+i+" .card-body").append('<div class="col-6">' + catalog[0].quantity + ' en stock</div>')
-        $(".cards"+i+" .card-body").append('<div class="col-12"> <button> Ajouter au panier </button> </div>')
-        console.log('test');
+        $(".card-container").append('<div class="cards-'+i+' col-12 col-md-6 col-lg-4">');
+        $('.cards-'+i).append('<div class="card-body row">');
+        $(".cards-"+i+" .card-body").append('<div class="col-12"> <a href="./product.html" class="text-decoration-none text-light"> </div>');
+        $(".cards-"+i+" .card-body a").append('<img src="'+ catalog[i].thumb +'" alt="" class="mw-100"></img>');
+        $(".cards-"+i+" .card-body a").append('<h4>'+ catalog[i].name +'</h4>');
+        $(".cards-"+i+" .card-body").append('<div class="col-6">' + catalog[i].price + ' €</div>');
+        $(".cards-"+i+" .card-body").append('<div class="col-6">' + catalog[i].quantity + ' en stock</div>');
+        $(".cards-"+i+" .card-body").append('<div class="col-12"> <button> Ajouter au panier </button> </div>');
+        
+    }
+
+
+    for (let i = 0; i < 2 ; i++){
+
+        if (i == 0){
+            $(".carousel-top").append('<div class="carousel-item active">');
+        }
+        else{
+            $(".carousel-top").append('<div class="carousel-item">');
+        }
+        $(".carousel-top .carousel-item:eq("+i+")").append('<a href="./product.html"></a>');
+        $(".carousel-top .carousel-item:eq("+i+") a").append('<img src="' + catalog[i].thumb + '" class="d-block w-100" alt="...">')
+        $(".carousel-top .carousel-item:eq("+i+") a").append('<p class="price-game mb-5 text-decoration-none text-warning">'+ catalog[i].price + ' €</p>')
         
     }
 
