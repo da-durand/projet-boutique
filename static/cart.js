113076068889053644757d33f677ad5c8e1540f7
tomsirat <sirat.tom@outlook.com>
2019-11-04 15:33:10 +0100

    Damien je te choisis

--- static/cart.js
+++ static/cart.js
@@ -1,53 +1,66 @@
 $(document).ready(function () {
 
+    var totals = [];
 
-    $(".cart-container").append("<div class='row article'>");
-    $(".article").append("<div class='article-header col-12 col-md-6'>");
-    $(".article-header").append("<h3>Titre</h3>");
-    $(".article-header").append('<img src="./static/img/image.png" alt="" srcset="" class="d-none d-md-block mw-100">');
-    $(".article").append('<div class="col-12 col-md-6 d-flex align-items-center details">');
-    $(".details").append('<div class="row d-flex align-items-center justify-content-between w-100 price-u">');
-    $(".price-u").append('<div class="col-4 col-md-4"> <p class="price-unite"> Prix unitaire </p> </div>');
+    function renderCart(i) {
 
-    $(".details").append('<div class="col-4 col-md-4 quantity-stock">');
-    $(".quantity-stock").append('<div class="row quantity-stock-container">');
+        var price = catalog[i].price
+        var quantity = cart[i].quantity
+        var total = price * quantity
+
+        $(".cart-container").append("<div class='row article'>");
+        $(".article:eq(" + i + ")").append("<div class='article-header col-12 col-md-6'>");
+        $(".article:eq(" + i + ") .article-header").append("<h3>" + catalog[i].name + "</h3>");// ICI
+
+        $(".article:eq(" + i + ") .article-header").append('<img src=' + catalog[i].thumb + ' alt="" srcset="" class="d-none d-md-block mw-100">');
+
+        $(".article:eq(" + i + ")").append('<div class="col-12 col-md-6 d-flex align-items-center details">');
+        $(".article:eq(" + i + ") .details").append('<div class="row d-flex align-items-center justify-content-between w-100 price-u">');
+        $(".article:eq(" + i + ") .price-u").append('<div class="col-4 col-md-4"> <p class="price-unite"> ' + catalog[i].price + ' €</p> </div>');
+        $(".article:eq(" + i + ") .details").append('<div class="col-4 col-md-4 quantity-stock">');
+        $(".article:eq(" + i + ") .quantity-stock").append('<div class="row quantity-stock-container">');
+        $(".article:eq(" + i + ") .quantity-stock-container").append('<div class="col-6 col-md-12"> <p class="d-md-none">Qt</p> <p class="d-none d-md-block">Quantité</p></div>');
+        $(".article:eq(" + i + ") .quantity-stock").append('<div class="col-6 col-md-12 d-flex justify-content-center"> <button>-</button> <p class="quantity"> ' + cart[i].quantity + ' </p> <button>+</button> </div>');// LA
+        $(".article:eq(" + i + ") .details").append('<div class="col-4 col-md-4">  <p class="total-price"> ' + total + ' €</p> </div>');
+
+        totals.push(total);
+
+        
 
 
-    
-
-    
 
 
+    };
+
+        console.log(totals);
+        
+
+
+
+
+
+    function renderTotal() {
+
+        $('.total-container').append('<div class="col-12 col-md-3 col-lg-2 cart-total-container">');
+        $('.cart-total-container').append('<div class="row cart-total">');
+        $('.cart-total').append(' <div class="col-6 col-md-12 cart-total-col">');
+        $('.cart-total-col').append('<div class="row cart-total-row">');
+        $('.cart-total-row').append('<div class="col-6 col-md-12"> <p>Total HTC</p> <p class="htc-price">233.33€</p> </div>');
+        $('.cart-total').append('<div class="col-6 col-md-12" > <p>Total TTC</p> <p class="ttc-price">280€</p> </div> ');
+        $('.cart-total').append('<div class="col-6 col-md-12"> <button>Payer</button> </div>');
+
+    }
+
+
+
+    // renderCart();
+    renderTotal();
+
+    for (let i = 0; i < cart.length; i++) {
+
+        renderCart(i)
+
+    }
 
 
 })
-
-    // < div class="row article" >
-    //     <div class="col-12 col-md-6">
-    //         <h3>Red Dead Redemption 2</h3>
-    //         <img src="./static/img/image.png" alt="" srcset="" class="d-none d-md-block mw-100">
-    //                         </div>
-    //         <div class="col-12 col-md-6 d-flex align-items-center">
-    //             <div class="row d-flex align-items-center justify-content-between w-100">
-    //                 <div class="col-4 col-md-4">
-    //                     <p class="price-unite"></p>
-    //                 </div>
-    //                 <div class="col-4 col-md-4">
-    //                     <div class="row">
-    //                         <div class="col-6 col-md-12">
-    //                             <p class="d-md-none">Qt</p>
-    //                             <p class="d-none d-md-block">Quantité</p>
-    //                         </div>
-    //                         <div class="col-6 col-md-12 d-flex justify-content-center">
-    //                             <button>-</button>
-    //                             <p class="quantity"></p>
-    //                             <button>+</button>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //                 <div class="col-4 col-md-4">
-    //                     <p class="total-price"></p>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
