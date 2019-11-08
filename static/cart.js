18e660bca030b705c2ffcdbbd4bd5988ba73b75e
dadurand <ha.halo@live.fr>
2019-11-08 12:03:32 +0100

    YOU WIN

--- static/cart.js
+++ static/cart.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     var cart = JSON.parse(sessionStorage.getItem("cart"));
     console.log(cart);
-    
+
 
     function renderCart(i) {
 
@@ -12,51 +12,51 @@ $(document).ready(function () {
         var quantity = cart[i].quantity;
         var total = price * quantity;
 
-        $(".cart-container").append("<div class='row article' id='art-"+i+"'>");
+        $(".cart-container").append("<div class='row article' id='art-" + cart[i].product_id + "'>");
         $(".article:eq(" + i + ")").append("<div class='article-header col-12 col-md-6'>");
         $(".article:eq(" + i + ") .article-header").append("<h3>" + catalog[cart[i].product_id].name + "</h3>");// ICI
 
         $(".article:eq(" + i + ") .article-header").append('<img src=' + catalog[cart[i].product_id].thumb + ' alt="" srcset="" class="d-none d-md-block mw-100">');
 
         $(".article:eq(" + i + ")").append('<div class="col-12 col-md-6 d-flex align-items-center details">');
-        $(".article:eq(" + i + ") .details").append("<button class='delete' id='del-"+i+"'>Supprimer</button>");
+        $(".article:eq(" + i + ") .details").append("<button class='delete' id='del-" + cart[i].product_id + "'>Supprimer</button>");
         $(".article:eq(" + i + ") .details").append('<div class="row d-flex align-items-center justify-content-between w-100 price-u">');
-        $(".article:eq(" + i + ") .price-u").append('<div class="col-4 col-md-4"> <p class="price-unite" id="price-' + i + '">' + catalog[cart[i].product_id].price + '</p><p>€</p> </div>');
+        $(".article:eq(" + i + ") .price-u").append('<div class="col-4 col-md-4"> <p class="price-unite" id="price-' + cart[i].product_id + '">' + catalog[cart[i].product_id].price + '</p><p>€</p> </div>');
         $(".article:eq(" + i + ") .details").append('<div class="col-4 col-md-4 quantity-stock">');
         $(".article:eq(" + i + ") .quantity-stock").append('<div class="row quantity-stock-container">');
         $(".article:eq(" + i + ") .quantity-stock-container").append('<div class="col-6 col-md-12"> <p class="d-md-none">Qt</p> <p class="d-none d-md-block">Quantité</p></div>');
-        $(".article:eq(" + i + ") .quantity-stock").append('<div class="col-6 col-md-12 d-flex justify-content-center"> <button id="minus-' + i +'" class="sub">-</button> <p class="quantity" id="sum-' + i + '"> ' + cart[i].quantity + ' </p> <button id="sup-' + i + '" class="sup">+</button> </div>');// LA
-        $(".article:eq(" + i + ") .details").append('<div class="col-4 col-md-4">  <p class="total-price" id="total-' + i + '">' + total + '</p><p>€</p> </div>');
+        $(".article:eq(" + i + ") .quantity-stock").append('<div class="col-6 col-md-12 d-flex justify-content-center"> <button id="minus-' + cart[i].product_id + '" class="sub">-</button> <p class="quantity" id="sum-' + cart[i].product_id + '"> ' + cart[i].quantity + ' </p> <button id="sup-' + cart[i].product_id + '" class="sup">+</button> </div>');// LA
+        $(".article:eq(" + i + ") .details").append('<div class="col-4 col-md-4">  <p class="total-price" id="total-' + cart[i].product_id + '">' + total + '</p><p>€</p> </div>');
 
         totals.push(total);
 
 
 
-    $(".sup").click(function(){
-        
-        var btn = $(this).attr("id");
-        var sum = btn.replace("sup", "sum");
-        
-        var tempQuantity = parseInt($("#"+sum).html());
-        
-        if (tempQuantity < catalog[cart[i].product_id].quantity) {
-            
-            var price = btn.replace("sup", "price");
-            var total = btn.replace("sup", "total");
-            
-            tempQuantity += 1;
-            $("#"+sum).html(tempQuantity);
-            
-            var priceRender = parseInt($("#"+price).html());
-            var sumRender = parseInt($("#"+sum).html());
-    
-            $("#"+total).html(priceRender * sumRender);
-    
-            renderTotal();
-            
-        }
-        
-    })
+        $(".sup").click(function () {
+
+            var btn = $(this).attr("id");
+            var sum = btn.replace("sup", "sum");
+
+            var tempQuantity = parseInt($("#" + sum).html());
+
+            if (tempQuantity < catalog[cart[i].product_id].quantity) {
+
+                var price = btn.replace("sup", "price");
+                var total = btn.replace("sup", "total");
+
+                tempQuantity += 1;
+                $("#" + sum).html(tempQuantity);
+
+                var priceRender = parseInt($("#" + price).html());
+                var sumRender = parseInt($("#" + sum).html());
+
+                $("#" + total).html(priceRender * sumRender);
+
+                renderTotal();
+
+            }
+
+        })
 
     };
 
@@ -65,7 +65,7 @@ $(document).ready(function () {
 
         var ttc = 0;
 
-        $(".total-price").each(function(){
+        $(".total-price").each(function () {
             ttc += parseFloat($(this).html());
         })
 
@@ -76,65 +76,78 @@ $(document).ready(function () {
         $('.cart-total').append(' <div class="col-6 col-md-12 cart-total-col">');
         $('.cart-total-col').append('<div class="row cart-total-row">');
         $('.cart-total-row').append('<div class="col-6 col-md-12"> <p>Total HTC</p> <p class="htc-price">' + htc + '</p> </div>');
-        $('.cart-total').append('<div class="col-6 col-md-12" > <p>Total TTC</p> <p class="ttc-price">'+ ttc +'</p> </div> ');
+        $('.cart-total').append('<div class="col-6 col-md-12" > <p>Total TTC</p> <p class="ttc-price">' + ttc + '</p> </div> ');
         $('.cart-total').append('<div class="col-6 col-md-12"> <button>Payer</button> </div>');
 
     }
-    
+
     for (let i = 0; i < cart.length; i++) {
-        
+
         renderCart(i);
-        
+
     }
-    
+
     renderTotal();
 
-    $(".sub").click(function(){
-            
-            var btn = $(this).attr("id");
-            var sum = btn.replace("minus", "sum");
+    $(".sub").click(function () {
 
-            tempQuantity = parseInt($("#"+sum).html());
-
-            if (tempQuantity != 0) {
-
-                var price = btn.replace("minus", "price");
-                var total = btn.replace("minus", "total");
-        
-                tempQuantity -= 1;
-                $("#"+sum).html(tempQuantity);
-                
-                var priceRender = parseInt($("#"+price).html());
-                var sumRender = parseInt($("#"+sum).html());
-        
-                $("#"+total).html(priceRender * sumRender);
-                
-                renderTotal();
-
-            }
-        
-    })
-    
-
-    $(".delete").click(function(){
         var btn = $(this).attr("id");
-        var article = btn.replace("del" , "art");
-        $("#" + article).remove();   
+        var sum = btn.replace("minus", "sum");
 
+        tempQuantity = parseInt($("#" + sum).html());
+
+        if (tempQuantity != 0) {
+
+            var price = btn.replace("minus", "price");
+            var total = btn.replace("minus", "total");
+
+            tempQuantity -= 1;
+            $("#" + sum).html(tempQuantity);
+
+            var priceRender = parseInt($("#" + price).html());
+            var sumRender = parseInt($("#" + sum).html());
+
+            $("#" + total).html(priceRender * sumRender);
+
+            renderTotal();
+
+        }
+
+    })
+
+    
+    $(".delete").click(function () {
+        var btn = $(this).attr("id");
+        var article = btn.replace("del", "art");
+        $("#" + article).remove();
+        
         renderTotal();
-    
-      var suppArticle = JSON.parse(sessionStorage.getItem("cart"));
+        
 
 
-            var idtest = parseInt(btn.replace("del-" , ""));
+        
+        var idItem = parseInt(btn.replace("del-", ""));
+        
+        
+        for (let i = 0; i < cart.length; i++) {
             
-            suppArticle[idtest]
-            
-          
-      
+            if(cart[i].product_id == idItem){
+                console.log("trololol");
+                
+                cart.splice(i, 1)
+                
+                sessionStorage.setItem("cart", JSON.stringify(cart))      
+            }     
+        }
+        
+        
 
-        console.log(suppArticle[idtest]);
-    
-             
+
+
+
+
+        console.log(cart);
+
+
     })
 })
